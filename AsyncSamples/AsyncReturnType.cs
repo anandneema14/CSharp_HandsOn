cddc44c08ef72b369e1cda787e026109e568b142
anand.neema14@gmail.com <anand.neema14@gmail.com>
2024-11-07 22:55:27 +0530

    Description about Task.Wait() and GetAwaiter()

---
+++ AsyncSamples/AsyncReturnType.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncSamples
+{
+    public class AsyncReturnType
+    {
+        public static Task MethodAsync1(int count)
+        {
+            Task task = new Task(() =>
+            {
+                for (int i = 1; i <= count; i++)
+                {
+                    Thread.Sleep(200);
+                    Console.WriteLine("Async Number: " + i);
+                }
+            });
+            task.Start();
+            return task;
+        }
+
+        public static Task<int> MethodAsync2(int count)
+        {
+            int result = 0;
+            Task<int> task = new Task<int>(() =>
+            {
+                for (int i = 1; i <= count; i++)
+                {
+                    Thread.Sleep(200);
+                    Console.WriteLine("Async Number: " + i);
+                    result += i;
+                }
+                return result;
+            });
+            task.Start();
+            return task;
+        }
+    }
+}
--- AsyncSamples/Program.cs
+++ AsyncSamples/Program.cs
@@ -4,15 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("--Sync Call");
+            Console.WriteLine("--Sync Call: Thread Example");
             // wait for the method call to be completed
             var syncResult = ThreadExample.MyMethodSync(4);
             Console.WriteLine(syncResult);
 
-            Console.WriteLine("--Async Call");
+            Console.WriteLine("--Async Call: Thread Example");
             // No wait for the method call to be completed
             var asyncResult = ThreadExample.MyMethodAsync(6);
             Console.WriteLine(asyncResult.Result);
+
+            Console.WriteLine("--Async Return Types");
+            
+            ///Task.GetAwaiter().GetResult() is preferred over Task.Wait and Task.Result
+            ///because it propagates exceptions rather than wrapping them in an AggregateException.
+            ///However, all these methods cause potential deadlock and thread pool starvation issues.
+            ///They should all be avoided in favor of async/await
+            var resultAsync2 = AsyncReturnType.MethodAsync2(5);
+            resultAsync2.Wait();
+            resultAsync2.GetAwaiter().GetResult();
+
 
             Console.ReadLine();
         }
