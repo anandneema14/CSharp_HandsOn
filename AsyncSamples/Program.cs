5859cbfd305b5fc3d2c463179e46034f88681e3e
anand.neema14@gmail.com <anand.neema14@gmail.com>
2024-11-07 18:30:27 +0530

    Async Sample. Implemented using Threading

---
+++ AsyncSamples/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AsyncSamples
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--Sync Call");
+            // wait for the method call to be completed
+            var syncResult = ThreadExample.MyMethodSync(4);
+            Console.WriteLine(syncResult);
+
+            Console.WriteLine("--Async Call");
+            // No wait for the method call to be completed
+            var asyncResult = ThreadExample.MyMethodAsync(6);
+            Console.WriteLine(asyncResult.Result);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AsyncSamples/ThreadExample.cs
@@ -0,0 +1,34 @@
+﻿namespace AsyncSamples
+{
+    public class ThreadExample
+    {
+        public static int MyMethodSync(int count)
+        {
+            int result = 0;
+            for (int i = 1; i < count; i++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("Sunc Number Print - " + i);
+                result += i;
+            }
+            return result;
+        }
+
+        public static Task<int> MyMethodAsync(int count)
+        {
+            Task<int> task = new Task<int>(() =>
+            {
+                int result = 0;
+                for (int i = 1; i < count; i++)
+                {
+                    Thread.Sleep(500);
+                    Console.WriteLine("Async Number Print - " + i);
+                    result += i;
+                }
+                return result;
+            });
+            task.Start(); //If we don't write this line there will be no result on the console
+            return task;
+        }
+    }
+}
