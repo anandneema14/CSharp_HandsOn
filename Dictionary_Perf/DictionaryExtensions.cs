9ddfcf22ac8e4c7f8a4f8c384f1962b808972814
anand.neema14@gmail.com <anand.neema14@gmail.com>
2025-01-22 17:32:58 +0530

    Dictionary Performance

---
+++ Dictionary_Perf/DictionaryExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary_Perf
+{
+    public static class DictionaryExtensions
+    {
+        /// <summary>
+        /// This is the method to add the value to the dictionary
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="dict"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static TValue? GetOrAdd<TKey, TValue>(
+            this Dictionary<TKey, TValue> dict, TKey key, TValue? value)
+            where TKey: notnull
+        {
+            ref var val = ref CollectionsMarshal.GetValueRefOrAddDefault(dict, key, out var exists);
+            if (exists)
+            {
+                return val;
+            }
+            val = value;
+            return val;
+        }
+
+        public static bool TryUpdate<TKey, TValue>(
+            this Dictionary<TKey, TValue> dict, TKey key, TValue value)
+            where TKey: notnull
+        {
+            ref var val = ref CollectionsMarshal.GetValueRefOrNullRef(dict, key);
+            if (Unsafe.IsNullRef(ref val))
+            {
+                return false;
+            }
+
+            val = value;
+            return true;
+        }
+    }
+}
---
+++ Dictionary_Perf/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Dictionary_Perf;
+using System.Text.Json;
+
+var values = new Dictionary<int, string>
+{
+    {24424,"Entry1" }
+};
+
+//Time complexity of using a dictionary is always O(1), meaning its constant time
+
+//to check the value before inserting it,
+//we need to traverse whole dictionary,
+//we are doing same hash operation multiple time as described in the code below
+ 
+if (values.ContainsKey(24424))
+{
+    values[24424] = "Test";
+}
+
+//Effective way
+var val = values.GetOrAdd(24424, "Test");
+Console.WriteLine(val);
+
+val = values.GetOrAdd(3452, "Test");
+Console.WriteLine(val);
+
+values.TryUpdate(24424, "Entry2");
+Console.WriteLine(JsonSerializer.Serialize(values));
+Console.ReadLine();
